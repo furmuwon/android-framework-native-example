fad573fcbcb6f14da5b2fb3b22650c4a24a036e3
furmuwon <furmuwon@gmail.com>
2016-05-23 22:29:27 +0900

    Android Thread Class Test, Add the DeathRecipient

--- framework/june/juneapp/main_june_app.cpp
+++ framework/june/juneapp/main_june_app.cpp
@@ -16,49 +16,126 @@
 
 #define LOG_TAG "juneapp"
 #define LOG_NDEBUG 0
+#include <utils/Log.h>
+
 
 #include <binder/IPCThreadState.h>
 #include <binder/ProcessState.h>
 #include <binder/IServiceManager.h>
-#include <utils/Log.h>
-
+#include <utils/threads.h>
 #include <include/IJune.h>
 
 using namespace android;
 
-sp<IJune> gJune;
-const sp<IJune>& get_june_service()
-{
-    if (gJune == 0) {
-        sp<IServiceManager> sm = defaultServiceManager();
-        sp<IBinder> binder;
-        do {
-            binder = sm->getService(String16("service.june"));
-            if (binder != 0)
-                break;
-            ALOGW("June not published, waiting...");
-            usleep(500000); // 0.5 s
-        } while (true);
-        gJune = interface_cast<IJune>(binder);
+class DemoApp : public Thread {
+public:
+    DemoApp()
+    {
+        ALOGV("DemoApp()");
+        mJune = 0;
+        mAppDathNotification = 0;
     }
-    ALOGE_IF(gJune==0, "no June!?");
 
-    return gJune;
-}
+    virtual ~DemoApp()
+    {
+        mJune.clear();
+        mAppDathNotification.clear();
+        ALOGV("~DemoApp()");
+    }
 
+    virtual status_t readyToRun()
+    {
+        mJune = get_june_service();
+        if (mJune == 0)
+            return NO_MEMORY;
+
+        return NO_ERROR;
+    }
+
+    virtual bool threadLoop()
+    {
+        while (!exitPending())
+        {
+            String8 desc;
+            Mutex::Autolock lock(appLock);
+            mJune->Init();
+            ALOGI("June Client Init");
+            desc = mJune->GetJuneServiceDesc();
+            ALOGI("June Client GetJuneServiceDesc [%s]", desc.string());
+            mJune->DeInit(0);
+            ALOGI("June Client DeInit");
+            usleep(1000000); // 1s
+        }
+
+        return false;
+    }
+
+    const sp<IJune> get_june_service()
+    {
+        ALOGV("get_june_service()");
+        Mutex::Autolock lock(appLock);
+        if (mJune == 0) {
+            sp<IServiceManager> sm = defaultServiceManager();
+            sp<IBinder> binder;
+            do {
+                binder = sm->getService(String16("service.june"));
+                if (binder != 0)
+                    break;
+                ALOGW("June not published, waiting...");
+                usleep(500000); // 0.5 s
+            } while (true);
+            if (mAppDathNotification == NULL) {
+                mAppDathNotification = new AppDathNotification(this);
+            } else {
+            }
+            binder->linkToDeath(mAppDathNotification, this);
+            mJune = interface_cast<IJune>(binder);
+        }
+        ALOGE_IF(mJune==0, "no June!?");
+
+        return mJune;
+    }
+
+    class AppDathNotification : public IBinder::DeathRecipient {
+    public:
+        AppDathNotification(DemoApp *app)
+        {
+            ALOGV("AppDathNotification!\n");
+            mApp = app;
+        }
+        virtual ~AppDathNotification(){}
+        // IBinder::DeathRecipient
+        virtual void binderDied(const wp<IBinder>& who)
+        {
+            ALOGW("Service Die\n");
+            mApp->requestExit();
+        }
+        DemoApp *mApp;
+    };
+
+    sp<IJune> mJune;
+    sp<AppDathNotification> mAppDathNotification;
+    mutable Mutex appLock;
+};
 
 int main(int argc, char** argv)
 {
-    String8 desc;
-    sp<IJune> june = get_june_service();
-    if (june == 0)
-        return -1;
+    ALOGV("Main DemoApp\n");
+    sp<ProcessState> proc(ProcessState::self());
+    android::ProcessState::self()->startThreadPool();
 
-    june->Init();
-    ALOGI("June Client Init");
-    desc = june->GetJuneServiceDesc();
-    ALOGI("June Client GetJuneServiceDesc [%s]", desc.string());
-    june->DeInit(0);
-    ALOGI("June Client DeInit");
+    sp<DemoApp> app = new DemoApp();
+    if (app == 0) {
+        ALOGE("DemoApp allocation fail\n");
+        return -1;
+    }
+    ALOGV("Run DemoApp\n");
+
+    app->run(); /* call the readyTorun in _threadLoop by calling run function */
+
+    ALOGV("Join DemoApp\n");
+    app->join();
+
     return 0;
 }
+
